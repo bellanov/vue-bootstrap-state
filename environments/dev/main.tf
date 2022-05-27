9fb9adbd4f2e47e13edb8655e99168bca5355cbc
Bellanov Apilli <BellanovApilli@gmail.com>
2022-05-26 22:08:03 -0400

    Initial dev environment

--- environments/dev/main.tf
+++ environments/dev/main.tf
@@ -11,5 +11,52 @@ terraform {
   }
 }
 
+# Query Relevant Project
+data "google_project" "vue-bootstrap-dev" {
+  project_id = "vue-bootstrap-dev"
+}
 
+# Deploy Cloud Run Service
+resource "google_cloud_run_service" "vue-bootstrap-cloudrun-dev" {
+  name     = "vue-bootstrap-cloudrun-dev"
+  location = "us-central1"
+  project = data.google_project.vue-bootstrap-dev.project_id
+
+  template {
+    spec {
+      containers {
+
+        image = "us-docker.pkg.dev/vue-bootstrap-dev/releases/vue-bootstrap:v1.0.14"
+
+        ports {
+          container_port = 80
+        }
+      }
+    }
+  }
+
+  traffic {
+    percent         = 100
+    latest_revision = true
+  }
+}
+
+# Policy - Allow Public Access
+data "google_iam_policy" "noauth" {
+  binding {
+    role = "roles/run.invoker"
+    members = [
+      "allUsers",
+    ]
+  }
+}
+
+# Grant Public Access
+resource "google_cloud_run_service_iam_policy" "noauth" {
+  location    = google_cloud_run_service.vue-bootstrap-cloudrun-dev.location
+  project     = google_cloud_run_service.vue-bootstrap-cloudrun-dev.project
+  service     = google_cloud_run_service.vue-bootstrap-cloudrun-dev.name
+
+  policy_data = data.google_iam_policy.noauth.policy_data
+}
 
--- environments/dev/outputs.tf
+++ environments/dev/outputs.tf
@@ -1,1 +1,7 @@
 
+
+# Cloud Run
+output "vue_bootstrap_cloudrun_dev" {
+  description = "Cloud Run - Dev."
+  value       = google_cloud_run_service.vue-bootstrap-cloudrun-dev
+}
