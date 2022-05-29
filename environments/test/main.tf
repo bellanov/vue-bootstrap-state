17a65cbb308150d3860e5abcf4a92d3bb9bc9509
Bellanov Apilli <BellanovApilli@gmail.com>
2022-05-29 15:24:59 -0400

    Created QA Environment

---
+++ environments/test/main.tf
@@ -0,0 +1,20 @@
+/**
+Create a Cloud Run service in the Development environment.
+*/
+
+terraform {
+  required_providers {
+    gcp = {
+      source  = "hashicorp/google"
+      version = "4.21.0"
+    }
+  }
+}
+
+module "cloud_run_service" {
+  source       = "../../modules/cloud_run_service"
+  project      = "${var.project}"
+  environment  = "${var.environment}"
+  image        = "${var.image}"
+  location     = "${var.location}"
+}
